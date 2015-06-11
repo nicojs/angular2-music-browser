65431b504226b59caf629466f213e2e3cdf0a1eb
Nico Jansen <jansennico@gmail.com>
2015-06-11 20:47:45 +0200

    Added temporary fix for ZoneJS issue in ArtistService

--- app/services/ArtistService.js
+++ app/services/ArtistService.js
@@ -6,7 +6,7 @@ var ArtistService = (function () {
     }
     ArtistService.prototype.getByGenre = function (genre) {
         genre = window['encodeURIComponent'](genre);
-        return window['fetch']("https://api.spotify.com/v1/search?q=genre:%22" + genre + "%22&type=artist")
+        return window['Zone'].bindPromiseFn(window['fetch'])("https://api.spotify.com/v1/search?q=genre:%22" + genre + "%22&type=artist")
             .then(function (response) { return response.json(); })
             .then(function (artistResponseJson) { return artistResponseJson.artists; })
             .then(function (artistResponse) { console.log(artistResponse); return artistResponse; });
--- app/services/ArtistService.ts
+++ app/services/ArtistService.ts
@@ -6,7 +6,10 @@ class ArtistService {
 
     getByGenre(genre: String) : any{
         genre = window['encodeURIComponent'](genre);
-        return window['fetch'](`https://api.spotify.com/v1/search?q=genre:%22${genre}%22&type=artist`)
+        
+        // Code in here is a temporary fix for a ZoneJs issue
+        // https://github.com/angular/angular/issues/1913
+        return window['Zone'].bindPromiseFn(window['fetch'])(`https://api.spotify.com/v1/search?q=genre:%22${genre}%22&type=artist`)
             .then(response => response.json())
             .then(artistResponseJson => artistResponseJson.artists)
             .then(artistResponse => {console.log(artistResponse); return artistResponse;});
